c45cc72ed7c622d88b4a9fd37cb201eb486f4d26
Илья <gagarinila@yandex.ru>
2022-09-15 19:12:22 +0500

    first_test

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class firstTest {
+
+    @Test
+    void summary(){
+        Assertions.assertTrue(5>2);
+    }
+}
